6c3d8213197077e0b1217d7d0bdad4fd3db1ce72
Rudra0911 <148303079+Rudra0911@users.noreply.github.com>
2024-09-11 17:05:47 +0530

    website
    view the website

---
+++ script.js
@@ -0,0 +1,7 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+rightnav=document.querySelector('.rightnav')
+
+burger.addEventlistener('click',()=>{
+    navbar,classlist.toggle('v-class-resp')
+})
